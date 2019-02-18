5d060fdc73cf79539c5aab59eafbee146f97e058
matthewgee <mattgee@gmail.com>
2019-02-17 21:18:09 -0600

    update site build script

--- site_build.sh
+++ site_build.sh
@@ -44,55 +44,11 @@ sed -i 's/INSERT_PROJECT_LEAD_ORG_NAME/$lead_organization/g'
 
 #Replace lead organization name
 grep -rli 'INSERT_PROJECT_PROJECT_TAGLINE' * | xargs -i@ 
-sed -i 's/INSERT_PROJECT_TAGLINE/$project_taglineg'
+sed -i 's/INSERT_PROJECT_TAGLINE/$project_tagline/g'
 
 
-#Create If loop to reverse config 
+echo "Remember to update the site_config.ini file with the rest of the site details."
 
-#Prompt to create each of the following: A github org, a project webiste url, a project S3 bucket, a project Gitter name, a Google Analytics tracking ID
-
-INSERT_PROJECT_GITHUB_ORG
-
-INSERT_PROJECT_URL
-
-INSERT_PROJECT_DOCS_URL
-
-INSERT_PROJECT_S3_URL
-
-INSERT_PROJECT_GITTER_NAME
-
-INSERT_GOOGLE_ANALYTICS_TRACKING_ID
-
-INSERT_CONSENT_TRACKING_JS_URL
-
-#Create a site twitter account
-INSERT_PROJECT_TWITTER_HANDLE
-
-#Create a mailchimp listserve and embed it in the home.html page
-INSERT_LIST_MANAGE_URL
-
-#Image Assets you should have
-
-#flavacon.icn
-INSERT_PROJECT_FLAVICON
-
-#Field Guide Image
-INSERT_PROJECT_FIELDGUIDE_IMAGE
-
-#Lead organization logo
-INSERT_PROJECT_LEAD_ORG_LOGO_URL
-
-INSERT_PRIVACY_POLICY_URL
-
-INSERT_TERMS_OF_USE_URL
-
-INSERT_PROJECT_DISCUSSION_FORUM
-
-#Create an events calendar on Google
-INSERT_EVENTS_CALENDAR_URL
-
-#Create a trello board for managing user stories
-INSERT_PROJECT_USER_STORY_TRELLO_BOARD
 
 #Add an explainer video to the banner by adding a `video.jpg` to the /content directory
 #Add a visual diagram explaining the project by adding a `diagram.png` to the /content directory
